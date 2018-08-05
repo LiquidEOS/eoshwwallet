48553dfbab8ce238bb813e30f6cb0784b98c0f1a
Tal Muskal <tal@bancor.network>
2018-08-05 14:18:36 -0700

    updates

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ var BDF = new bdf();
 var BDFBig = new bdf();
 var gpiop = gpio.promise;
 var http = require('http');
+import Eos from 'eosjs'
+let {ecc, Fcbuffer} = Eos.modules;
 
 var bg1 = "Qk3eDwAAAAAAAD4AAAAoAAAAegAAAPoAAAABAAEAAAAAAAAAAAASCwAAEgsAAAIAAAACAAAA/////wAAAP8AAAAAAAAAAAAAAAAAAAAAAAAAAAAtoAAAAAAAAAAAAAAAAAAD//wAAAAAAAAAAAAAAAAAD///gAAAAAAAAAAAAAAAAD///+AAAAAAAAAAAAAAAAD////4AAAAAAAAAAAAAAAB/////AAAAAAAAAAAAAAAB/////4AAAAAAAAAAAAAAA//////AAAAAAAAAAAAAAAf/////4AAAAPAAAAAAAAAf//9///AAAAH4AAAAAAAAP//wD//4AAAD/AAAAAAAAD//AAD//AAAD/4AAAAAAAD//gAAf/wAAA//AAAAAAAA//gAAD/+AAAP/wAAAAAAAf/wAAAP/wAAB/8AAAAAAAP/4AAAB/8AAAP/AAAAAAAH/8AAAAP/gAAB/4AAAAAAB/8AAAAD/8AAAf+AAAAAAA//AAAAAf/AAAD/gAAAAAAP/gAAYAD/4AAA/4AAAAAAH/wAAHwA//AAAP+AAAAAAB/4AAB+AH/wAAD/gAAAAAA/+AAAfwA/+AAA/4AAAAAAP/AAAH+AH/wAAP+AAAAAAH/gAAB/gB/+AAH/gAAAAAB/4AAAf4AP/gAB/4AAAAAAf+AAAP+AD/+AA/8AAAAAAH/AAAD/gAf/wAf/AAAAAAD/wAAB/4AD//AP/wAAAAAA/8AAA/+AAf////4AAAAAAP+AAAP/AAH////+AAAAAAD/gAAH/wAA/////AAAAAAA/4AAD/4AAH////gAAAAAAP+AAB/+AAA////wAAAAAAD/gAA//AAAH///8AAAAAAB/8AAf/gAAAf//8AAAAAAAP+AAP/4AAAD//8AAAAAAAD/wAP/8AAAAP/+AAAAAAAAf+AP/+AAAAAv8AAAAAAAAP/6f//AAAAAAAAAAAAAAAB/////gAAAAAAAAAAAAAAAf////wAAAAAAAAAAAAAAAD////4AAAAAAAAAAAAAAAAf///8AAAAAAAAAAAAAAAAD///8AAAAAAAAAAAAAAAAA///+AAAAAAAAAAAAAAAAAD//+AAAAAAKAAAAAAAAAAAP/+AAAAAH//AAAAAAAAAAAv4AAAAAP///AAAAAAAAAAAAAAAAAf///8AAAAAAAAAAAAAAAAf////gAAAAAAAAAAAAAAAf////+AAAAAAAAAAAAAAAf/////wAAAAAAAAAAAAAAH/////+AAAAAAAAAAAAAAH//////wAAAAAAAAAAAAAD//////+AAAAAAAAAAAAAA///93//gAAAAAAAAAAAAA//+AAf/8AAAAAAAAAAAAAP/8AAA//AAAAAAAAAAAAAH/4AAAH/4AAAAAAAAAAAAB/8AAAAf+AAAAAAAAAAAAA/8AAAAH/gAAAAAAAAAAAAP+AAAAA/8AAAAAAAAAAAAD/AAAAAH/AAAAAAAAAAAAA/wAAAAB/wAAAAAAAAAAAAP8AAAAAP8AAAAAAAAAAAAD/AAAAAH/AAAAAAAAAAAAA/4AAAAA/wAAAAAAAAAAAAP/AAAAAP8AAAAAAAAAAAAD/9AAAAD/AAAAAAAAAAAAA//8AAAA/wAAAAAAAAAAAAH//gAAAP8AAAAAAAAAAAAA//wAAAH/AAAAAAAAAAAAAP/8AAAB/gAAAAAAAAAAAAB//AAAAf4AAAAAAAAAAAAAH/gAAAP+AAAAAAAAAAAAAA/4AAAH/AAAAAAAAAAAAAABQAAAB/wAAAAAAAAAAAAAAAAAAA/8AAAAAAAAAAAAAAAAAAA/+AAAAAAAAAAAAAAAAAAAf/gAAAAAAAAAAAAAAAAAE3/wAAAAAAAAAAAAAAAAA///4AAAAAAAAAAAAAAAAH///8AAAAAAAAAAAAAAAAD////AAAAAAAAAAAAAAAAH////4AAAAAAAAAAAAAAAH/////AAAAAAAAAAAAAAAD/////4AAAAAAAAAAAAAAD//////AAAAAAAAAAAAAAB//////4AAAAAAAAAAAAAA//////+AAAAAAAAAAAAAAf//0A//wAAAAAAAAAAAAAP//AAB/8AAAAAAAAAAAAAH//AAAP/AAAAAAAAAAAAAB/+AAAB/wAAAAAAAAAAAAA//AAAAP8AAAAAAAAAAAAAf/gAAAD/AAAAAAAAAAAAAH/gAAAA/wAAAAAAAAAAAAD/wAAAAP8AAAAAAAAAAAAA/8AAAAD/AAAAAAAAAAAAAf+AAAAB/gAAAAAAAAAAAAH/AAAAA/4AAAAAAAAAAAAB/wAAAAf8AAAAAAAAAAAAA/4AAAAP/AAAAAAAAAAAAAP+H/7u//gAAAAAAAAAAAAD/H/////wAAAAAAAAAAAAA/w/////8AAAAAAAAAAAAAP8f/////gAAAAAAAAAAAAD+D/////8AAAAAAAAAAAAA/w//////AAAAAAAAAAAAAP4P/////4AAAAAAAAAAAAD/B//////AAAAAAAAAAAAA/gP/////wAAAAAAAAAAAoH8AqqtP/8AAAAAAAAAAA/h/AAAAAP/AAAAAAAAAAAP4DAAAAAB/wAAAAAAAAAAB/gAAAAAAP8AAAAAAAAAAA/wAAAAAAD/AAAAAAAAAAAH+AAAAAAA/wAAAAAAAAAAD/gAAAAAAf4AAAAAAAAAAAf4AAAAAAH+AAAAAAAAAAAP+AAAAAAD/gAAAAAAAAAAB/gAAAAAD/wAAAAAAAAAAA/4AAAAAD/4AAAAAAAAAAAH+AAAAAH/+AAAAAAAHAAAB/gAAAA///AAAAAAAB/////////////gAAAAAAAf////////////4AAAAAAAP/////////////AAAAAAAB/////////////8AAAAAAAf/////////////AAAAAAAH/////////////4AAAAAAB/////////////+AAAAAAAP/////////////wAAAAAAB/////////////8AAAAAAAAhCRf4gQERCJf/AAAAAAAAAAAP8AAAAAAB/wAAAAAAAAAAD/gAAAAAAf8AAAAAAAAAAAf4AAAAAAD/AAAAAAAAAAAP+AAAAAAA/wAAAAAAAAAAB/AAAAAAAP8AAAAAAAAAAA/4AAAAAAH+AAAAAAAAAAAP+AAAAAAD/gAAAAAAAAAAB/gAAAAAB/4AAAAAAAAAAA/4AAAAABf8AAAAAAAAAAAH+AAAAAB/+AAAAAAACAAAB/gAAAAH//AAAAAAAB7VKq/6UyqqX//gAAAAAAAf////////////wAAAAAAAH/////////////AAAAAAAD/////////////4AAAAAAAf/////////////AAAAAAAH/////////////4AAAAAAB/////////////+AAAAAAAP/////////////wAAAAAAB/////////////8AAAAAAAP3d3/7bt7e3f//AAAAAAAAAAAH+AAAAAAD/wAAAAAAAAAAD/gAAAAAAf8AAAAAAAAAAA/4AAAAAAD/AAAAAAAAAAAP+AAAAAAA/wAAAAAAAAAAB/gAAAAAAP8AAAAAAAAAAAf4AAAAAAD/AAAAAAAAAAAP+AAAAAAB/gAAAAAAAAAAB/gAAAAAA/4AAAAAAAAAAAf4AAAAAA/+AAAAAAAAAAAH+AAAAAAv+AAAAAAAAAAAB/gAAAEQ//gAAAAAAAAAAAf4AAD////wAAAAAAAAAAAH+AAH////4AAAAAAAAAAAB/gAP////8AAAAAAAAAAAAf4AP////8AAAAAAAAAAAAH+AP/////AAAAAAAAAAAAD/gH/////4AAAAAAAAAAAAfwD//////AAAAAAAAAAAAP+D//////4AAAAAAAAAAAB/g///////AAAAAAAAAAAA/4f//9///4AAAAAAAAAAAH+P/+AH//+AAAAAAAAAAAD/H/8AB///wAAAAAAAAAAA/5/8AAfx/+AAAAAAAAAAAP4/8AAf8P/gAAAAAAAAAADQP+AAP+B/4AAAAAAAAAAAAD/AAH/gP/AAAAAAAAAAAAA/wAD/4B/wAAAAAAAAAAAAP8AD/8Af8AAAAAAAAAAAAD/gD/+AD/AAAAAAAAAAAAA/////AA/wAAAAAAAAAAAAP////wAP8AAAAAAAAAAAAD////4AD/AAAAAAAAAAAAAf///8AA/wAAAAAAAAAAAAH///8AAP8AAAAAAAAAAAABf//+AAD/AAAAAAAAAAAAAP///AAB/wAAAAAAAAAAAAA///AAAP4AAAAAAAAAAAAAH/+AAAH+AAAAAAAAAAAAAAPcAAAD/gAAAAAAAAAAAAAAAAAAA/4AAAAAAAAAAAAAAAAAAAf8AAAAAAAAAAAAAAAAAAAP/AAAAAAAAAAAAAAAAAAAH/wAAAAAAAAAAAAPgAAAAH/4AAAAAAAAAAAAP+AAAAD/8AAAAAAAAAAAAD/wAAAH/+AAAAAAAAAAAAB/+AAAP//gAAAAAAAAAAAAf/wAL///wAAAAAAAAAAAAH///////wAAAAAAAAAAAAD///////4AAAAAAAAAAAAAf//////8AAAAAAAAAAAAAH//////4AAAAAAAAAAAAAB//////8AAAAAAAAAAAAAAP/////wAAAAAAAAAAAAAAB////8AAAAAAAAAAAAAAAAP//8gAAAAAAAAAAAAAAAAB/8AAAAAAAAAAAAAAAAAAAH/AAAAAAAAAAAAAAAAAAAB/wAAAAAAAAAAAAAAAAAAAf8AAAAAAAAAAAAAAAAAAAH/AAAC6AAAAAAAAAAAAAAB/wAA//+AAAAAAAAAAAAAAP8AD///4AAAAAAAAAAAAAH/AP////AAAAAAAAAAAAAB/wf////4AAAAAAAAAAAAAf8f/////AAAAAAAAAAAAAH///////4AAAAAAAAAAAAB///////+AAAAAAAAAAAAAf///////wAAAAAAAAAAAAD/////f/8AAAAAAAAAAAAB////QAf/AAAAAAAAAAAAAP//6AAB/wAAAAAAAAAAAAB//wAAAP8AAAAAAAAAAAAAP/AAAAD/AAAAAAAAAAAAAA8AAAAA/wAAAAAAAAAAAAAAAAAAAP8AAAAAAAAAAAAAAAAAAAH/AAAAAAAAAAAAAAAAAAAB/gAAAAAAAAAAAAAAAAAAA/4AAAAAAAAAAAAAAAAAAAf8AAAAAAAAAAAAAAAAAAAf/AAAAAAAAAAAAAAAAAAA//gAAAAAAAAAAAAAAAAAB//wAAAAAAAAAAAAAAAAAH//4AAAAAAAAAAAAAAAAAD//8AAAAAAAAAAAAAAAAAB//8AAAAAAAAAAAAAAAAAAP/+AAAAAAAAAAAAAAAAAAD/+AAAAAAAAAAAAAAAAAAAf8AAAAAAAAAAAAAAAAAAAD4AAAAAAAAAAAAAAAAAAAAAAAAAAAA";
 var bg2 = "Qk3eDwAAAAAAAD4AAAAoAAAAegAAAPoAAAABAAEAAAAAAAAAAAASCwAAEgsAAAIAAAACAAAA/////wAAAP/////////////////////A///////SX///////////wP/////+AAP//////////8D/////8AAAf//////////A/////8AAAB//////////wP////8AAAAH/////////8D////+AAAAA//////////A////+AAAAAH/////////wP///+AAAAAA/////////8D////gAAAAAH////4////A////wAACAAA////4H///wP///wAAX+AAH///4A///8D///4AAf/8AB///+AH///A///+AAf//gAP///AB///wP///AAf//8AB///wAP//8D///gAP///wAP//+AD///A///wAP///+AD///wA///wP//4AD////gAf//+AH//8D//+AB////8AD///gB///A///AB/////gA///8Af//wP//wAf//3/8AH///AH//8D//4AP//4P/AA///wB///A//+AH//+B/4AP//8Af//wP//AB///gP/AB///AH//8D//wA///4B/4AP//wB///A//4AP//+Af+AB//4Af//wP/+AH///gH/wAP/+AH//8D//gB///wB/8AB//AD///A//4A///8Af/gAP/gA///wP/8AP//+AH/8AA/gAf//8D//AD///AD//gABAAH///A//wB///wA//8AAAAB///wP/8Af//4AP//AAAAA///8D//AH//8AH//4AAAAf///A//wB//+AB///AAAAP///wP/8Af//AA///8AAAH///8D//AH//gAf///gAAD////A//wA//wAH///8AAB////wP/8AP/wAD////wAD////8D//AB/wAB/////gF/////A//4ACgAA////////////wP/+AAAAAf///////////8D//gAAAAP////////////A//8AAAAH////////////wP//gAAAD////////////8D//8AAAD/////////////A///gAAB/////////////wP//8AAB//////1//////8D///wAB/////4AA//////A////gH/////wAAB/////wP//////////gAAAH////8D//////////gAAAAP////A//////////gAAAAB////wP/////////wAAAAAP///8D/////////wAAAAAB////A/////////4AAAAAAP///wP////////8AAAAAAD///8D/////////AAABAAAf///A/////////AAB//gAD///wP////////wAD///AA///8D////////4AH///8AH///A////////+AD////AB///wP////////AD////8Af//8D////////wB/////AD///A////////8A/////4A///wP////////AP////+AP//8D////////wD/////wD///A////////8A/////8A///wP////////AH/////AP//8D////////wB/////wD///A////////8AC////8A///wP////////AAD////AP//8D////////4AA////wD///A////////+AAH///4A///wP////////wAD///+Af//8D////////+AA////gD///A/////////4Af///wB///wP/////////AH///4A///8D/////////+v///+AP///A///////////////AD///wP//////////////gB///8D//////////////gAf///A/////////////9QAP///wP///////////+AAAH///8D///////////8AAAD////A///////////0AAAA////wP//////////8AAAAH///8D//////////4AAAAA////A//////////8AAAAAH///wP/////////8AAAAAA///8D/////////+AAAAAAH///A//////////AAAAAAB///wP/////////gAAN/AAf//8D/////////wAA//+AD///A/////////4AA///wA///wP////////+AB///+AP//8D/////////AA////wD///A/////////gAf///8A///wP////////4Af////AP//8D////////8AP////wD///A/////////AD////8A///wP////////wB////+Af//8D////////4A/////AH///A////////+AP////gD///wP////////gH////wA///8D////////wBwAoRAAf///A////////8A8AAAAAP///wP////////AOAAAAAD///8D////////wDwAAAAAf///A////////8A8AAAAAD///wP///////+AfAAAAAAf//8D////////wDwAAAAAH///A////////8A+AAAAAB///wP////////AfwAAAAAP//8D//////+v4D/VWqyAD///A///////g+A/////wA///wP//////wD8/////+AP//8D//////+Af//////wD///A///////AP//////8A///wP//////4B///////AP//8D//////8Af//////gD///A///////gH//////4B///wP//////4B//////8Af//8D//////8Af/////8AP///A///////gH/////8AD///wP//////4B/////4AD///8D//4///+Af////AAA////A//+AAAAAAAAAAAAAf///wP//gAAAAAAAAAAAAH///8D//wAAAAAAAAAAAAA////A//+AAAAAAAAAAAAAD///wP//gAAAAAAAAAAAAA///8D//4AAAAAAAAAAAAAH///A//+AAAAAAAAAAAAAB///wP//wAAAAAAAAAAAAAP//8D//+AAAAAAAAAAAAAD///A////f3vAH//fu9ugA///wP//////4B//////+AP//8D//////8Af//////gD///A///////AH//////8A///wP//////4B///////AP//8D//////8Af//////wD///A///////gH//////4B///wP//////wB//////8Af//8D//////+Af//////AH///A///////AH/////+AD///wP//////4B/////+AB///8D//9///+Af////8AA////A//+FSragFra2tgAAf///wP//gAAAAAAAAAAAAP///8D//4AAAAAAAAAAAAA////A//8AAAAAAAAAAAAAH///wP//gAAAAAAAAAAAAA///8D//4AAAAAAAAAAAAAH///A//+AAAAAAAAAAAAAB///wP//wAAAAAAAAAAAAAP//8D//+AAAAAAAAAAAAAD///A///whEIACEkUkSRAA///wP//////4B//////8AP//8D//////8Af//////gD///A///////AH//////8A///wP//////wB///////AP//8D//////+Af//////wD///A///////gH//////8A///wP//////4B//////+Af//8D//////+Af//////AH///A///////gH//////AD///wP//////wB//////gA///8D//////+Af///7fAAf///A///////gH//8AAAAP///wP//////4B//4AAAAH///8D//////+Af/wAAAAD////A///////gH/wAAAAD////wP//////4B/4AAAAA////8D//////8Af4AAAAAH////A///////gH8AAAAAA////wP//////wB8AAAAAAH///8D//////+AfAAAAAAA////A///////AHgAACAAAH///wP//////4BwAB/8AAB///8D//////8A4AD/+AAAP///A///////AGAD//AOAD///wP//////wHgD//gD4Af//8D//////8fwB//4A+AH///A////////8A//4AfwA///wP////////AP/8AP+AP//8D////////wD/8AD/gD///A////////8Af8AA/8A///wP////////AAAAA//AP//8D////////wAAAAP/wD///A////////8AAAAP/8A///wP////////gAAAD//AP//8D////////4AAAB//wD///A/////////AAAB//8A///wP////////wAAA///AP//8D/////////AAA///gH///A/////////4AB///4A///wP/////////yO///+Af//8D///////////////AH///A///////////////gD///wP//////////////wA///8D//////////////4AP///A////////wf////4AH///wP///////wB////8AD///8D///////8AP///4AB////A///////+AB///wAAf///wP///////gAP/oAAAP///8D///////4AAAAAAAP////A///////8AAAAAAAH////wP///////gAAAAAAD////8D///////4AAAAAAH/////A///////+AAAAAAD/////wP///////wAAAAAP/////8D///////+AAAAD///////A////////oAADf///////wP////////AD/////////8D////////4A//////////A////////+AP/////////wP////////gD/////////8D////////4A///9V/////A////////+AP/+gAB////wP////////gD/6AAAH///8D////////8A/4AAAA////A////////+APgAAAAH///wP////////gDAAAAAA///8D////////4AAAAAAAH///A////////+AAAAAAAB///wP////////gAAAAAAAP//8D////////8AAAAAgAD///A/////////AAAAv/wA///wP////////wAAF//+AP//8D////////+AAP///wD///A/////////4A////8A///wP////////+D/////AP//8D///////////////wD///A///////////////4A///wP//////////////+Af//8D///////////////AH///A///////////////gD///wP//////////////gA///8D//////////////gAf///A/////////////8AAP///wP////////////4AAH///8D////////////8AAD////A////////////+AAD////wP////////////wAB////8D////////////8AB/////A/////////////gD/////wP////////////8H/////8D////////////////////A";
@@ -344,135 +346,17 @@ class ConfirmationMessage {
 	}
 
 	drawOptions(){				
-		const {title,choices,text} = this.options;
+		const {title,choices,text, text2} = this.options;
 		clear(false);
 		drawText(3,3,title, true);
 		for(var i=0; i < choices.length ; i++){
 			drawBox(3+ i * 13,15, 15 ,20,true, i === this.selection);
 			drawText(3+i*13+1,25,choices[i].toString(), i !== this.selection);
-		}
-
-        var textToDraw = text;
-		drawText(50,70,textToDraw,true);
+		}        
+		drawText(20,65,text,true);
+		drawText(20,80,text2,true);
 	}
 }
-function genSeed(){
-	let examplePhrase = 'witch collapse practice feed shame open despair creek road again ice least';
-		examplePhrase = "witch collapse";
-		const words = examplePhrase.split(' ');
-		return words;
-}
-async function showSeed(words){
-		for (var i = 0; i <= words.length - 1; i++) {
-			var word = words[i];
-			clear(false);
-			drawText(3,3,(i+1) + ". " + word, true);
-			await delay(4000);
-		}
-
-}
-async function genShowSeed(pw){
-		clear(false);
-		drawText(3,3,"generating seed", true);
-		await delay(4000);
-		// showing seed.			
-		clear(false);
-		drawText(3,3,"showing seed:", true);
-		console.log('showing seed');
-		await delay(4000);		
-		const words = genSeed();
-		await showSeed(words);
-		clear(false);
-		console.log('showing seed again');
-		drawText(3,3,"showing seed again:", true);
-		await delay(4000);
-		await showSeed(words);
-
-		console.log("proceed?")
-		// do you want to proceed?
-		const confirm = new ConfirmationMessage({
-			title: "proceed?",
-			text: "did you write it down?",
-			onSelect: async (yn)=>{
-				if(yn == 'Y'){
-					// proceed
-					clear(false);
-					// verify password
-					drawText(3,3,"verifying", true);
-					await delay(4000);
-					clear(true);
-					drawText(3,3,"unlocked", false);
-					// await delay(4000);
-					// clear(false);
-					// unlocked
-					currentUI = null;
-					unlocked = true;
-					rebootTimer = new RebootTimer();					
-				}
-				else {
-					// restart process
-					clear(false);
-					drawText(3,3,"generating new seed", true);
-					await delay(4000);
-					genShowSeed(pw);
-				}
-
-			}
-		});		
-		confirm.start();
-}
-
-function startSelectPW(){
-	var selectPw = new InputMessage({
-		title: "choose password",
-		onSelect: async (pw)=>{
-			selectPw = new InputMessage({
-				title: "verify password",
-				onSelect: async (pw2)=>{
-					if(pw != pw2){
-						clear(false);
-						drawText(3,3,"passwords do not match", true);
-						await delay(4000);
-						startSelectPW();
-					}
-					else{
-						genShowSeed(pw);
-					}					
-				},
-				hide: true,
-				choices: [1,2,3,4,5,6,7,8,9,0,'A','B','C','D','E','F','<','!']
-			});
-			selectPw.start();
-		},
-		hide: true,
-		choices: [1,2,3,4,5,6,7,8,9,0,'A','B','C','D','E','F','<','!']
-	});
-	selectPw.start();
-}
-
-
-const delay = time => new Promise(res=>setTimeout(()=>res(),time));
-var rebootTimer;
-var enterPw = new InputMessage({
-	title: "enter password",
-	hide: true,
-	onSelect: async (pw)=>{
-		clear(false);
-		// verify password
-		drawText(3,3,"verifying", true);
-		await delay(4000);
-		clear(true);
-		drawText(3,3,"unlocked", false);
-		// await delay(4000);
-		// clear(false);
-		// unlocked
-		currentUI = null;
-		unlocked = true;
-		rebootTimer = new RebootTimer();
-	},
-	choices: [1,2,3,4,5,6,7,8,9,0,'A','B','C','D','E','F','<','!']
-});
-
 
 function buttonL(){
 	if(lastTimes.l){
@@ -584,7 +468,6 @@ draw();
 clear(true);
 
 var currentUI;
-startSelectPW();
 
 
 
@@ -601,24 +484,22 @@ app.post('/', async (req, res) => {
 		clear(false);
 		drawText(0,0,"click to unlock", true);
 		res.send('locked');
-		return 
-
+		return;
 	}
-
 	const confirm = new ConfirmationMessage({
 		title: "sign?",
-		text: req.body.text,
+		text: req.body.text || "",
+		text2: req.body.text2 || "",
 		onSelect: async (yn)=>{
-				if(yn == 'Y'){
-					// proceed
-					await delay(300);
+				if(yn == 'Y'){					
+					var signature = ecc.sign(Buffer.from(req.body.data, 'base64'), privateKey);
+
 					clear(false);
 					drawText(0,0,"signed", true);
-					res.send("signed transaction.");
+					res.send("signed transaction: " + signature);
 					currentUI = null;
 				}
 				else {
-					// restart process
 					
 					clear(false);
 					drawText(0,0,"rejected", true);
@@ -636,3 +517,174 @@ app.post('/', async (req, res) => {
 // sign transaction
 
 app.listen(3000, () => console.log('Wallet listening on port 3000!'))
+
+const hdkey = require('hdkey')
+const wif = require('wif')
+const ecc = require('eosjs-ecc')
+const bip39 = require('bip39')
+
+function genSeed(pw){	
+    let mnemonic = bip39.generateMnemonic();
+    mnemonic = genMnemonicWithPass(pwd);
+    // mnemonic = bip39.generateMnemonic();
+	const words = mnemonic.split(' ');	
+	return words;
+}
+async function showSeed(words){
+		for (var i = 0; i <= words.length - 1; i++) {
+			var word = words[i];
+			clear(false);
+			drawText(3,3,(i+1) + ". " + word, true);
+			await delay(4000);
+		}
+
+}
+async function genShowSeed(pw){
+		clear(false);
+		drawText(3,3,"generating seed", true);
+		await delay(4000);
+		// showing seed.			
+		clear(false);
+		drawText(3,3,"showing seed:", true);
+		console.log('showing seed');
+		await delay(4000);		
+		const words = genSeed();
+		await showSeed(words);
+		clear(false);
+		console.log('showing seed again');
+		drawText(3,3,"showing seed again:", true);
+		await delay(4000);
+		await showSeed(words);
+
+		console.log("proceed?")
+		// do you want to proceed?
+		const confirm = new ConfirmationMessage({
+			title: "proceed?",
+			text: "did you write it down?",
+			onSelect: async (yn)=>{
+				if(yn == 'Y'){
+					// proceed
+					clear(false);
+					// verify password
+					drawText(3,3,"verifying", true);
+					await delay(4000);
+
+					// await delay(4000);
+					// clear(false);
+					// unlocked
+					currentUI = null;
+					unlockWallet(genMnemonicWithPass(pw));
+
+				}
+				else {
+					// restart process
+					clear(false);
+					drawText(3,3,"generating new seed", true);
+					await delay(4000);
+					genShowSeed(pw);
+				}
+
+			}
+		});		
+		confirm.start();
+}
+
+var publicKey = '';
+var privateKey = '';
+function genMnemonicWithPass(password){
+	const hash = await secureHash(password);
+    let mnemonic = bip39.entropyToMnemonic(hash);
+    return bip39.mnemonicToSeedHex(mnemonic);
+}
+function unlockWallet(mnemonic){
+	unlocked = true;
+	rebootTimer = new RebootTimer();
+	const seed = bip39.mnemonicToSeedHex(mnemonic)
+	const master = hdkey.fromMasterSeed(Buffer(seed, 'hex'))
+	const node = master.derive("m/44'/194'/0'/0/0")
+	publicKey = ecc.PublicKey(node._publicKey).toString();
+	privateKey = wif.encode(128, node._privateKey, false);
+	clear(true);
+	drawText(3,3,"unlocked", false);
+	drawText(3,30,publicKey, false);
+}
+
+async function secureHash(cleartext) {
+	const scrypt = require('scrypt-async');
+    return new Promise(async resolve => {
+        const salt = await StorageService.getSalt();
+        scrypt(cleartext, salt, {
+            N: 16384,
+            r: 8,
+            p: 1,
+            dkLen: 16,
+            encoding: 'hex'
+        }, (derivedKey) => {
+            resolve(derivedKey);
+        })
+    });
+}
+
+function startSelectPW(){
+	var selectPw = new InputMessage({
+		title: "choose password",
+		onSelect: async (pw)=>{
+			selectPw = new InputMessage({
+				title: "verify password",
+				onSelect: async (pw2)=>{
+					if(pw != pw2){
+						clear(false);
+						drawText(3,3,"passwords do not match", true);
+						await delay(4000);
+						startSelectPW();
+					}
+					else{
+						fs.writeFileSync('/home/pi/wallet.inited', "");
+						genShowSeed(pw);
+					}					
+				},
+				hide: true,
+				choices: [1,2,3,4,5,6,7,8,9,0,'A','B','C','D','E','F','<','!']
+			});
+			selectPw.start();
+		},
+		hide: true,
+		choices: [1,2,3,4,5,6,7,8,9,0,'A','B','C','D','E','F','<','!']
+	});
+	selectPw.start();
+}
+
+
+const delay = time => new Promise(res=>setTimeout(()=>res(),time));
+var rebootTimer;
+var enterPw = new InputMessage({
+	title: "enter password",
+	hide: true,
+	onSelect: async (pw)=>{
+		clear(false);
+		// verify password
+		drawText(3,3,"verifying", true);
+		await delay(4000);
+
+		// await delay(4000);
+		// clear(false);
+		// unlocked
+		var mnemonic = '';
+		mnemonic = genMnemonicWithPass(pw);
+		unlockWallet();
+		currentUI = null;
+		
+	},
+	choices: [1,2,3,4,5,6,7,8,9,0,'A','B','C','D','E','F','<','!']
+});
+
+
+var passwordExist = fs.existsSync('/home/pi/wallet.inited');
+if(passwordExist){
+	enterPw.start();
+}
+else{
+	startSelectPW();
+}
+
+
