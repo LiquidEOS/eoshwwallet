185671ac81a20143d7f776329bab592d52b4afbd
Tal Muskal <tal@bancor.network>
2018-08-07 12:06:21 -0700

    switching to SSD1306 OLED

--- index.js
+++ index.js
@@ -427,22 +427,25 @@ function makeWatcher (pin) {
 }
 var buttons = [];
 
-if(!isDebug){
-	const L_pin = 27 
-	const R_pin = 23 
-	const C_pin = 4 
-	const U_pin = 17 
-	const D_pin = 22 
-	const A_pin = 5 
-	const B_pin = 6 
-	var pins = [L_pin, R_pin, C_pin, U_pin, D_pin, A_pin, B_pin];
-	for (var i=0; i<pins.length; i++) {
-            buttons[i] = new gpio.DigitalInput({
-                pin:'GPIO'+pins[i],
-                pullResistor: gpio.PULL_UP});
-            buttons[i].on ('change', makeWatcher (pins[i]));
-    }
 
+if(!isDebug){
+	var raspi = require ('raspi');
+	raspi.init (() => {
+		const L_pin = 27 
+		const R_pin = 23 
+		const C_pin = 4 
+		const U_pin = 17 
+		const D_pin = 22 
+		const A_pin = 5 
+		const B_pin = 6 
+		var pins = [L_pin, R_pin, C_pin, U_pin, D_pin, A_pin, B_pin];
+		for (var i=0; i<pins.length; i++) {
+	            buttons[i] = new gpio.DigitalInput({
+	                pin:'GPIO'+pins[i],
+	                pullResistor: gpio.PULL_UP});
+	            buttons[i].on ('change', makeWatcher (pins[i]));
+	    }
+	}
 
 	// gpio.setMode(gpio.MODE_BCM);
 	// gpio.setup(5, gpio.DIR_IN, gpio.EDGE_BOTH);
