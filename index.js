6f48fc1986c8718e5f14ac7bd088e9337dce36f5
Tal Muskal <tal@bancor.network>
2018-08-07 14:35:10 -0700

    switching to SSD1306 OLED

--- index.js
+++ index.js
@@ -274,9 +274,10 @@ class InputMessage {
 		
 		console.log("draw",this.currentLine, (i < choices.length) , (i <= (this.currentLine+1) * 9));
 
-		for(var i=this.currentLine * 9; (i < choices.length) && (i <= (this.currentLine+1) * 9); i++){
-			drawBox(3+ i * 13,15, 10 ,10,true, i === this.selection);			
-			drawText(3+i*13+1,17,choices[i].toString(), i !== this.selection);
+		for(var i=this.currentLine * 9; (i < choices.length) && (i < (this.currentLine+1) * 9); i++){
+			var j = i % 9;
+			drawBox(3+ j * 13,15, 10 ,10,true, i === this.selection);
+			drawText(3+j*13+1,17,choices[i].toString(), i !== this.selection);
 		}
 
         var textToDraw = '';
