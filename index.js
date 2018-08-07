15eab0eead926cdc637ffde9915d654aa1b5c12e
Tal Muskal <tal@bancor.network>
2018-08-07 12:15:49 -0700

    switching to SSD1306 OLED

--- index.js
+++ index.js
@@ -416,6 +416,7 @@ var buttons = [];
 if(!isDebug){
 	var raspi = require ('raspi');
 	raspi.init (() => {
+		console.log("inited");
 		const L_pin = 27 
 		const R_pin = 23 
 		const C_pin = 4 
@@ -425,6 +426,7 @@ if(!isDebug){
 		const B_pin = 6 
 		var pins = [L_pin, R_pin, C_pin, U_pin, D_pin, A_pin, B_pin];
 		for (var i=0; i<pins.length; i++) {
+				console.log("initing", pins[i]);
 	            buttons[i] = new gpio.DigitalInput({
 	                pin:'GPIO'+pins[i],
 	                pullResistor: gpio.PULL_UP});
