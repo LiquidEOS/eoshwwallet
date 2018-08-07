aca8fbb0f77234eb388f91fb2ae9ee6ae1ae9484
Tal Muskal <tal@bancor.network>
2018-08-07 11:58:42 -0700

    switching to SSD1306 OLED

--- index.js
+++ index.js
@@ -464,10 +464,36 @@ gpio.on('change', function(channel, value) {
 
 	}
 });
+
+function makeWatcher (pin) {
+    return function (val) {
+    			console.log(pin,val);
+                // handleButton (pin, val);
+            }
+}
+    var buttons = [];
+
 if(!isDebug){
-	gpio.setMode(gpio.MODE_BCM);
-	gpio.setup(5, gpio.DIR_IN, gpio.EDGE_BOTH);
-	gpio.setup(6, gpio.DIR_IN, gpio.EDGE_BOTH);
+	const L_pin = 27 
+	const R_pin = 23 
+	const C_pin = 4 
+	const U_pin = 17 
+	const D_pin = 22 
+	const A_pin = 5 
+	const B_pin = 6 
+	var pins = [L_pin, R_pin, C_pin, U_pin, D_pin, A_pin, B_pin];
+for (var i=0; i<pins.length; i++) {
+//                console.log ("making GPIO" + pins[i])
+                buttons[i] = new gpio.DigitalInput({
+                    pin:'GPIO'+pins[i],
+                    pullResistor: gpio.PULL_UP});
+                buttons[i].on ('change', makeWatcher (pins[i]));
+            }
+
+
+	// gpio.setMode(gpio.MODE_BCM);
+	// gpio.setup(5, gpio.DIR_IN, gpio.EDGE_BOTH);
+	// gpio.setup(6, gpio.DIR_IN, gpio.EDGE_BOTH);
 	// L_pin = 27 
 	// R_pin = 23 
 	// C_pin = 4 
