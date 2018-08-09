5c6aa1652750d86a67fd9856ac09e93fe588af5a
Tal Muskal <tal@bancor.network>
2018-08-08 17:40:49 -0700

    logos

--- index.js
+++ index.js
@@ -135,8 +135,8 @@ function drawLine(x0,y0,x1,y1,c){
 }
 async function drawImage(file){
 	return pixelBitmap.parse(file).then(function(images){
-	  console.log(images[0]);
-	  oled.drawBitmap(images[0]);
+	  console.log(images.length);
+	  oled.drawBitmap(images[0][0]);
 	  oled.update();
 	});
 }
